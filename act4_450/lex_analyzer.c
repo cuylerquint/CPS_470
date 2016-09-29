07420e9308a971df349f11e1305e914c81e9f11d
cuylerquint <deanquint@gmail.com>
2016-09-29 13:28:28 -0400

    different routine of reading and parsing

--- act4_450/lex_analyzer.c
+++ act4_450/lex_analyzer.c
@@ -25,41 +25,110 @@ Act 4
 #include <ctype.h>
 
 
+void keyw(char *p);
+int i=0,id=0,kw=0,num=0,op=0;
+char keys[32][10]={"auto","break","case","char","const","continue","default",
+"do","double","else","enum","extern","float","for","goto",
+"if","int","long","register","return","short","signed",
+"sizeof","static","struct","switch","typedef","union",
+"unsigned","void","volatile","while"};
+
 
 int main(int argc, char **argv){
-
+	char ch,str[25],seps[15]=" \t\n,;(){}[]#\"<>",oper[]="!%^&*-+=~|.<>/?";
+	int j;
+	char fname[50];
 
 	FILE *fi, *fo;
 	fi = fopen(argv[1], "r");
 	fo = fopen(argv[2], "w+");
-   	Node * root = 0;
 	char line[2000];
 	int line_c = 0;
-	if(fi != NULL)
+
+	while((ch=fgetc(fi))!=EOF)
 	{
-		while(fgets(line,sizeof line , fi) !=NULL){
-			line_c++;
-			char * pch;
-  			pch = strtok (line," ,?\n");
-  			while (pch != NULL)
-  			{
-    				size_t ln = strlen(pch) - 1;
-    				if (*pch  && pch[ln] == '\n') 
-					pch[ln] = '\0';
-    				if (*pch  && pch[ln-1] == '?') 
-    					pch[ln-1] = '\0';
-				int *freq = malloc(sizeof *freq);
-				freq = insertNode(&root, pch, 0,line_c);
-    				pch = strtok (NULL, " ,.-");
-  			}	
-
+		for(j=0;j<=14;j++)
+		{
+			if(ch==oper[j])
+			{
+				printf("%c is an operator\n",ch);
+				op++;
+				str[i]='\0';
+				keyw(str);
+			}
 		}
-		fclose(fi);
+		for(j=0;j<=14;j++)
+		{
+			if(i==-1)
+				break;
+			if(ch==seps[j])
+			{
+				if(ch=='#')
+				{
+					while(ch!='>')
+					{
+						printf("%c",ch);
+						ch=fgetc(fi);
+					}
+					printf("%c is a header file\n",ch);
+					i=-1;
+					break;
+				}
+				if(ch=='"')
+				{
+					do
+					{
+						ch=fgetc(fi);
+						printf("%c",ch);
+					}while(ch!='"');
+					printf("\b is an argument\n");
+					i=-1;
+					break;
+				}
+				str[i]='\0';
+				keyw(str);
+			}
+		}
+		if(i!=-1)
+		{
+			str[i]=ch;
+			i++;
+		}		
+		else
+			i=0;
 	}
-	print(fo,root);
-   	fclose(fo);
-	return 0;
-
+	printf("Keywords: %d\nIdentifiers: %d\nOperators: %d\nNumbers: %d\n",kw,id,op,num);
+//getch();
 }
-
-
+void keyw(char *p)
+{
+	int k,flag=0;
+	for(k=0;k<=31;k++)
+	{
+		if(strcmp(keys[k],p)==0)
+		{
+			printf("%s is a keyword\n",p);
+			kw++;
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+	{
+		if(isdigit(p[0]))
+		{
+			printf("%s is a number\n",p);
+			num++;
+		}
+		else
+		{
+			//if(p[0]!=13&&p[0]!=10)
+			if(p[0]!='\0')
+			{
+				printf("%s is an identifier\n",p);
+				id++;
+			}
+		}
+	}
+	i=-1;
+}
